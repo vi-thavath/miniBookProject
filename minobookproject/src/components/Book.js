6921d21b291799eb48ac65f90dd01e85f11d126e
vi-thavath <vithavath.yeng@gmail.com>
2022-06-28 07:25:39 +0700

    another commit

--- minobookproject/src/components/Book.js
+++ minobookproject/src/components/Book.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
 class Book extends Component {
+  constructor(props) {
+    super(props);
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  state = {
+    count: 0,
+  };
+
+  // handleClick() {
+  //   console.log("you clicked me");
+  //   console.log(this.state.count);
+  // }
+
+  handleClick = () => {
+    console.log("you click me");
+    console.log(this.state.count);
+  };
   render() {
     // const { book, author } = this.props.info;
     const { img, title, author } = this.props.info;
@@ -10,6 +28,7 @@ class Book extends Component {
         <div>
           <h4>TITLE:{title}</h4>
           <h6>BY:{author}</h6>
+          <button onClick={this.handleClick()}>Add count</button>
         </div>
       </div>
     );
